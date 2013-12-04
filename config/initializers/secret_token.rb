66744ba193230489c2926798481581a08b7a9e55
Alex Weidmann <alex@twinsoftech.com>
2013-12-03 18:18:16 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CryptoGroupBuy::Application.config.secret_key_base = '89fb2c39114cada790038c400840468bd4915ab3bb1c66626ddc0ebee344e988f3d0ecc3bded2c93e6026923dbbc435e5dc4870a5f5ac1b67e7631e45d6c44f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CryptoGroupBuy::Application.config.session_store :cookie_store, key: '_crypto_group_buy_session'
